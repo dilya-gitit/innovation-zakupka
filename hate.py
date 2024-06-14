a63c379ea48748716cf531a419d9fb0bd2b1a420
Dilnas Yerkemblan <d.yerkemblan@ptc-holding.kz>
2024-06-14 15:39:37 +0500

    Boonmm

--- hate.py
+++ hate.py
@@ -147,7 +147,8 @@ def main():
                 chrome_options.add_argument("--disable-gpu")
                 chrome_options.add_argument("--no-sandbox")
                 chrome_options.add_argument("--disable-dev-shm-usage")
-                
+                chrome_options.add_experimental_option("detach", True)
+
                 # Use ChromeDriverManager to manage the ChromeDriver installation
                 driver_path = ChromeDriverManager().install()
                 service = Service(driver_path)
